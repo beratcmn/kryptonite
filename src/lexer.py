37a73f27489aae07ca02939d19a4d8409638552e
beratcmn <beratcmn@gmail.com>
2023-04-19 05:35:07 +0300

    Added print function

--- src/lexer.py
+++ src/lexer.py
@@ -1,7 +1,28 @@
 from dataclasses import dataclass
 from classes.Line import Line
+from helpers.string2int import StringToken
+from keywords import KEYWORDS, COMMENT_PREFIX
 
 
 @dataclass
 class Lexer:
-    pass
+    string_maps: list[StringToken]
+
+    def lex(self, lines: list[Line]) -> list[Line]:
+        """Lexes the lines."""
+
+        for line in lines:
+            line.text = line.text.strip()
+            if line.text.startswith(COMMENT_PREFIX):
+                break
+
+            for keyword in KEYWORDS:
+                if line.text.startswith(keyword):
+                    line.text = line.text.replace(keyword, KEYWORDS[keyword])
+
+            for string_token in self.string_maps:
+                line.text = line.text.replace(
+                    str(string_token.int_index), f'"{string_token.value}"'
+                )
+
+        return lines
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from helpers import string2int
 from helpers.string2int import StringToken
 from classes.Line import Line
 from reader import Reader
+from lexer import Lexer
 
 import sys
 
@@ -12,15 +13,21 @@ def main(args: list):
     file_path = args[0]
 
     lines = Reader().readInputFile(file_path)
-    string_tokens = []
+
+    string_maps = []
 
     for line in lines:
         tokens = string2int.convert(line.text)
-        string_tokens.extend(tokens)
+        string_maps.extend(tokens)
         for string_token in tokens:
             line.text = line.text.replace(
                 f'"{string_token.value}"', str(string_token.int_index)
             )
+
+    lexer = Lexer(string_maps=string_maps)
+    lines = lexer.lex(lines)
+
+    print(lines)
 
 
 if __name__ == "__main__":
