e00f2f6ea57601a6ba2030a2d71d8594b0550d32
beratcmn <beratcmn@gmail.com>
2023-04-19 05:17:22 +0300

    Simple keyword pairs

---
+++ src/keywords.py
@@ -0,0 +1,10 @@
+KEYWORDS = {
+    "yazdır": "print",
+    "eğer": "if",
+    "değilse": "else",
+    "fonksiyon": "function",
+    # "döngü": "while",
+    # "yap": "do",
+    # "yapı": "struct",
+    # "döndür": "return",
+}
